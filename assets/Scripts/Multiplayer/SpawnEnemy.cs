d452c0058886b47ca70ac8690d75af5fb45a7f6d
fdinsen <gammarik@gmail.com>
2022-03-15 13:07:12 +0100

    expands spawn enemies script to spawn continously

--- assets/Scripts/Multiplayer/SpawnEnemy.cs
+++ assets/Scripts/Multiplayer/SpawnEnemy.cs
@@ -5,22 +5,49 @@ using Photon.Pun;
 
 public class SpawnEnemy : MonoBehaviour
 {
+    [SerializeField] private bool ActivateEnemySpawning = true;
+    [SerializeField] private float secondsBetweenSpawns = 10f;
+    [SerializeField] private int numberOfEnemiesToSpawn = 100;
     [SerializeField] private GameObject enemyPrefab;
+    [SerializeField] private Vector3[] _spawnLocations;
+
 
     private Vector3 spawnOffset = new Vector3(2, 0, 0);
+
+    
     // Start is called before the first frame update
     void Start()
     {
         if(PhotonNetwork.IsMasterClient)
         {
-            PhotonNetwork.Instantiate(enemyPrefab.name, transform.position, Quaternion.identity);
-            PhotonNetwork.Instantiate(enemyPrefab.name, transform.position - spawnOffset, Quaternion.identity);
+            StartCoroutine(SpawnEnemiesLoop());
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SpawnOneEnemy()
     {
-        
+        Vector3 randomLocation;
+        if (_spawnLocations.Length == 0)
+        {
+            randomLocation = transform.position;
+        }
+        else
+        {
+            randomLocation = _spawnLocations[Random.Range(0, _spawnLocations.Length)];
+        }
+
+        PhotonNetwork.Instantiate(enemyPrefab.name, randomLocation, Quaternion.identity);
+    }
+
+    public IEnumerator SpawnEnemiesLoop()
+    {
+        int enemiesSpawned = 0;
+        while(enemiesSpawned < numberOfEnemiesToSpawn)
+        {
+            SpawnOneEnemy();
+            enemiesSpawned++;
+            yield return new WaitForSeconds(secondsBetweenSpawns);
+        }
+        Debug.Log("FINISHED");
     }
 }
