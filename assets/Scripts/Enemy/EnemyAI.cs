2fd236ae7772182e98bd93b1c939c773c70f874c
fdinsen <gammarik@gmail.com>
2022-03-16 11:11:25 +0100

    Fixes enemyAI getting stuck on walls

--- assets/Scripts/Enemy/EnemyAI.cs
+++ assets/Scripts/Enemy/EnemyAI.cs
@@ -6,7 +6,7 @@ using CodeMonkey.Utils;
 using System;
 using Photon.Pun;
 
-[RequireComponent(typeof(NavMeshAgent), typeof(EnemyShooting))]
+[RequireComponent(typeof(NavMeshAgent), typeof(EnemyShooting), typeof(CircleCollider2D))]
 public class EnemyAI : MonoBehaviourPunCallbacks, IPunObservable
 {
     private enum State
@@ -15,15 +15,30 @@ public class EnemyAI : MonoBehaviourPunCallbacks, IPunObservable
     }
 
     [Header("Layer Filters")]
-    [SerializeField] private LayerMask blockedLocations;
-    [SerializeField] private ContactFilter2D targetFilter;
+    [SerializeField] [Tooltip("Layers which count as unwalkable and will result in a new roaming position to be chosen. Example: Walls")]
+    private LayerMask blockedLocations;
+    [SerializeField] [Tooltip("The filter used to finding targets. Example use: Set LayerMask to Player, to make the enemy chase players.")]
+    private ContactFilter2D targetFilter;
 
     [Header("AI Variables")]
-    [SerializeField] private float sightRange = 10f;
+    [SerializeField] [Tooltip("The distance within the player has to be for the enemy to spot them.")]
+    private float sightRange = 10f;
     [SerializeField] private float attackRange = 3f;
-    [SerializeField] private float loseSightDistance = 20f;
-    [SerializeField] private float loiterTimeMin = .5f;
-    [SerializeField] private float loiterTimeMax = 1.5f;
+    [SerializeField] [Tooltip("The distance which the player being chased has to be outside for the enemy to lose sight of them.")]
+    private float loseSightDistance = 20f;
+    [SerializeField] [Tooltip("The minimum range for standing still between roaming moves, in seconds.")]
+    private float loiterTimeMin = .5f;
+    [SerializeField] [Tooltip("The maximum range for standing still between roaming moves, in seconds.")]
+    private float loiterTimeMax = 1.5f;
+
+    [Header("Stuck Detection")]
+    [SerializeField] [Tooltip("If the enemy doesn't move more than this distance for the given amount of frames, it counts as it being stuck.")] 
+    private float _stuckDetectionDistance = 0.01f;
+    [SerializeField] [Tooltip("The amount of frames the character has to be unmoving for for it to count as being stuck.")]
+    private int _stuckDetectionFrameLimit = 30;
+    
+    private int _stuckDetectionFrames = 0;
+    private Vector3 _stuckPositionCache;
 
     private State _state;
     private NavMeshAgent _agent;
@@ -32,6 +47,8 @@ public class EnemyAI : MonoBehaviourPunCallbacks, IPunObservable
     private Animator _anim;
     private Vector3 _startingPos;
     private Vector3 _roamPos;
+
+
 
     private GameObject _currentTarget;
     private PhotonView _targetView;
@@ -47,6 +64,7 @@ public class EnemyAI : MonoBehaviourPunCallbacks, IPunObservable
     // Start is called before the first frame update
     void Start()
     {
+        _stuckPositionCache = transform.position;
         _anim = GetComponentInChildren<Animator>();
         _shooter = GetComponent<EnemyShooting>();
         _health = GetComponent<EnemyHealth>();
@@ -56,6 +74,7 @@ public class EnemyAI : MonoBehaviourPunCallbacks, IPunObservable
         _agent.updateRotation = false;
         _startingPos = transform.position;
         _roamPos = GetRoamingPosition();
+        targetFilter.useLayerMask = true;
         if (_health) _health.EnemyDied += _ => _state = State.Dead;
 
         _agent.SetDestination(_roamPos);
@@ -92,7 +111,6 @@ public class EnemyAI : MonoBehaviourPunCallbacks, IPunObservable
 
     private void Roam()
     {
-        
         _anim.SetFloat("Speed", GetVector2Size(_agent.velocity));
         float reachedPositionDistance = 2f;
         if (Vector3.Distance(transform.position, _roamPos) < reachedPositionDistance)
@@ -101,12 +119,17 @@ public class EnemyAI : MonoBehaviourPunCallbacks, IPunObservable
             _timeUntilStopLoitering = Time.time + UnityEngine.Random.Range(loiterTimeMin, loiterTimeMax);
             _state = State.Loitering;
         }
+        if (VectorHasNotChangedWithinTime(transform.position))
+        {
+            // immediately stop loitering. Effectively just finds a new roaming position.
+            _timeUntilStopLoitering = Time.time;
+            _state = State.Loitering;
+        }
         FindTarget();
     }
 
     private void Loiter()
     {
-        
         if (Time.time > _timeUntilStopLoitering)
         {
             _roamPos = GetRoamingPosition();
@@ -147,7 +170,7 @@ public class EnemyAI : MonoBehaviourPunCallbacks, IPunObservable
     public Vector3 GetRoamingPosition()
     {
         var loc = _startingPos + UtilsClass.GetRandomDir() * UnityEngine.Random.Range(3f, 10f);
-        if (Physics2D.OverlapCircle(loc, 1f, blockedLocations))
+        if (Physics2D.OverlapCircle(loc, 2f, blockedLocations))
         {
             loc = GetRoamingPosition();
         }
@@ -205,5 +228,25 @@ public class EnemyAI : MonoBehaviourPunCallbacks, IPunObservable
     private float GetVector2Size(Vector2 v)
     {
         return Mathf.Abs(v.x) + Mathf.Abs(v.y);
+    }
+
+    private bool VectorHasNotChangedWithinTime(Vector3 currentVector)
+    {
+        if (!VectorsAreWithinRange(_stuckPositionCache, currentVector, _stuckDetectionDistance))
+        {
+            _stuckPositionCache = transform.position;
+            return false;
+        }
+        if (_stuckDetectionFrames >= _stuckDetectionFrameLimit)
+        {
+            return true;
+        }            
+        _stuckDetectionFrames++;
+        return false;
+    }
+
+    private bool VectorsAreWithinRange(Vector3 oldVector, Vector3 newVector, float range)
+    {
+        return Vector3.Distance(oldVector, newVector) < range;
     }
 }
